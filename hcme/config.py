77ba1df46676434f2404b25fab6307e7d94232b3
Thomas Tu <thomasthetu@gmail.com>
2021-11-16 14:51:43 -0800

    new artifacts, new settings!

--- hcme/config.py
+++ hcme/config.py
@@ -12,7 +12,7 @@ _settings = Dynaconf(
     warn_dynaconf_global_settings=True,
     environments=True,
     default_env="hcme",
-    lowercase_read=False,
+    lowercase_read=True,
     default_settings_paths=constants.DEFAULT_SETTINGS_FILES,
 )
 
@@ -40,3 +40,17 @@ input_data = _settings.get("input_files")
 # Insert a path before each key
 for k, v in input_data.items():
     input_data[k] = input_dir / v
+
+# Registry for artifacts relative to output_dir
+artifacts = _settings.get("artifacts")
+
+for k, v in artifacts.items():
+    _path = output_dir / v
+    if create_directories:
+        if _path.is_dir():
+            _path.mkdir(parents=True, exist_ok=True)
+        else:
+            _path.parent.mkdir(parents=True, exist_ok=True)
+    artifacts[k] = _path
+
+reference_date = _settings.get("reference_date", "2021-03-15")
--- hcme/utils.py
+++ hcme/utils.py
@@ -1,5 +1,7 @@
-from typing import Iterable
+from typing import Iterable, List
 from pathlib import Path
+
+from hcme.db import Base
 
 
 def assert_depends_on(depends_on: Iterable[Path]) -> None:
@@ -8,3 +10,10 @@ def assert_depends_on(depends_on: Iterable[Path]) -> None:
     """
     for f in depends_on:
         assert f.exists(), f"{f} does not exist"
+
+
+def get_nullable_columns(model: Base) -> List[str]:
+    """Return a list of nullable columns in a model."""
+    return list(
+        map(lambda c: c.name, filter(lambda c: c.nullable, model.__table__.columns))
+    )
