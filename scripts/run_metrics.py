49429f5122f98074292b2d6cb9edaf338e82bdf9
Thomas Tu <thomasthetu@gmail.com>
2022-04-11 22:37:02 -0700

    only run top 7

--- scripts/run_metrics.py
+++ scripts/run_metrics.py
@@ -8,7 +8,7 @@ from hcme.metrics import export
 
 if __name__ == "__main__":
 
-    demand_matrix = summary.DemandMatrix(top_n=10)
+    demand_matrix = summary.DemandMatrix(top_n=7)
     demand_matrix.run()
 
     # Export data
